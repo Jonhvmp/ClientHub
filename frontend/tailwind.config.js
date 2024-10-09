d7a619d26374de5ac98b17f4f21dcf547eaf7a87
Jonh Alex <122692601+Jonhvmp@users.noreply.github.com>
2024-10-09 01:37:58 -0300

    Fixing tailwindcss rendering paths

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,8 @@
 module.exports = {
   content: [
     './src/**/*.{html,js,jsx,ts,tsx}', // Certifique-se de incluir todos os arquivos onde Tailwind será usado
+    './src/assets/css/**/*.css', // Inclua todos os arquivos .css dentro da pasta src/assets/css
+    // Global css
     './src/assets/css/global.css', // Inclua o arquivo global.css
   ],
   theme: {
